3b97e018e41d90373c5e65ed90bdba48053d6f87
snithyasri80bcs28-cloud <snithyasri80_bcs28@mepcoeng.ac.in>
2026-07-29 15:52:34 +0530

    Update CountingBits.c

--- IMPLEMENTATION OF FRAMING PROTOCOLS/CountingBits.c
+++ IMPLEMENTATION OF FRAMING PROTOCOLS/CountingBits.c
@@ -15,3 +15,18 @@ int* countBits(int n, int* returnSize) {
     }
     return ans;
 }
+/*Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101*/
