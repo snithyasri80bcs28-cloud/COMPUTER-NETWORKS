aafc3f450146c35dada4558cb070aa63ad77dec8
snithyasri80bcs28-cloud <snithyasri80_bcs28@mepcoeng.ac.in>
2026-07-29 14:51:47 +0530

    Create CountingBits.c

---
+++ IMPLEMENTATION OF FRAMING PROTOCOLS/CountingBits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* countBits(int n, int* returnSize) {
+    *returnSize = n + 1;
+    int* ans = (int*)malloc((*returnSize) * sizeof(int));
+    for (int i = 0; i <= n; i++) {
+        int count = 0;
+        int num = i;
+        while (num > 0) {
+            count += (num % 2); 
+            num /= 2;           
+        }
+        ans[i] = count;
+    }
+    return ans;
+}
