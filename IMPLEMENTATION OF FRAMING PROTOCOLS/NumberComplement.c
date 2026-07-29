0d48b3cb14974e464c71966f66c7eaca9a9596a0
snithyasri80bcs28-cloud <snithyasri80_bcs28@mepcoeng.ac.in>
2026-07-29 15:54:59 +0530

    Update NumberComplement.c

--- IMPLEMENTATION OF FRAMING PROTOCOLS/NumberComplement.c
+++ IMPLEMENTATION OF FRAMING PROTOCOLS/NumberComplement.c
@@ -10,3 +10,9 @@ int findComplement(int num) {
     // XOR flips the bits where mask has 1s
     return num ^ mask;
 }
+/*Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.*/
